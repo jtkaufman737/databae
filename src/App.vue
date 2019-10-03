118f81ffe0bc3e3bc523becf8525566273090ac9
jtk-codes <jt.kaufman@nutanix.com>
2019-10-03 18:46:24 -0400

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+nav.navbar {
+  background-color:white;
+  height:150px;
+  width:100%;
+}
+
+html {
+  background-color:black;
+  height:100vh;
+  width:100vw;
+  font-family:'Be Vietnam', sans-serif;
+}
+
+body {
+  height:100%;
+  width:100%;
+  margin:0px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <nav class="navbar">
+      <img src="../assets/databae.png"/>
+    </nav>
+    <section class="postlist">
+
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+  methods: {},
+
+  beforeMount() {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  height:100px;
+  margin:auto;
+}
+
+nav.navbar {
+  display:flex;
+  vertical-align:center;
+  justify-content:flex-end;
+}
+</style>
